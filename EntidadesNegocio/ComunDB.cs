b069081ccd6aaba9edfc8fbcdf69c7468feece3d
AngelaEspinosa <titagena@gmail.com>
2022-05-01 16:44:06 -0500

    Se crea la clase ComanDB dentro de la capa de BaseDatos.

---
+++ EntidadesNegocio/ComunDB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace AirSur.BaseDatos
+{
+    public class ComunDB
+    {
+        const string StrConexion = @"Data Source=conflictosaisur.database.windows.net;Persist Security Info=True;User ID=adminaisu;Password=@isur1234";
+
+        private static SqlConnection obtenerConexion()
+        {
+            SqlConnection connection = new SqlConnection(StrConexion);
+            connection.Open();
+            return connection;
+        }
+
+        public static SqlCommand obtenerComando()
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = obtenerConexion();
+            return command;
+        }
+
+        public static int ejecutarComando(SqlCommand pComando)
+        {
+            int resultado = pComando.ExecuteNonQuery();
+            pComando.Connection.Close();
+            return resultado;
+        }
+
+        public static SqlDataReader ejecutarComandoReader(SqlCommand pComando)
+        {
+            SqlDataReader reader = pComando.ExecuteReader(CommandBehavior.CloseConnection);
+            return reader;
+        }
+    }
+}
---
+++ EntidadesNegocio/SoldadoDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using AirSur.EntidadesDeNegocio;
+
+namespace AirSur.BaseDatos
+{
+    public class SoldadoDAL
+    {
+        public static int guardar(Soldado pSoldado)
+        {
+            string consulta = "INSERT INTO soldado(edad,capacidadCarga,idPais_Fk,idProfesion_Fk,idRango_Fk) VALUES(@edad,@experiencia,@capacidadCarga,@idPais_Fk,@idProfesion_Fk,@idRango_Fk)";
+            SqlCommand comando = ComunDb.
+        }
+    }
+}

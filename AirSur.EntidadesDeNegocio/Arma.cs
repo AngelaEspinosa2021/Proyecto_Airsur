a00520d280bfabef06f944484837d843303f54e7
jccarden23 <jimeingsistemas@gmail.com>
2022-05-03 22:55:11 -0500

    Creacion clases  en entidadNegocio Arma, TipoArma, Clasifiacion, Marca y creación inset y listar de Arma

---
+++ AirSur.EntidadesDeNegocio/Arma.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AirSur.EntidadesDeNegocio
+{
+    public class Arma
+    {
+        [Key]
+        public int Id_arma { get; set; }
+        public string Nombre { get; set; }
+        public int Peso { get; set; }
+        public int CantidadMunicion { get; set; }
+        [ForeignKey("TipoArma")]
+        public int IdTipoArma_Fk { get; set; }
+        [ForeignKey("Marca")]
+        public int IdMarca_Fk { get; set; }
+    }
+}
